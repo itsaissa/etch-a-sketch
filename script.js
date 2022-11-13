4dcffe4479de1bc91173f7281234435a8cd57b45
ItsaIssa <44656132+itsaissa@users.noreply.github.com>
2022-11-12 16:00:44 -0800

    Add grid size change functionality

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+const GRID_SIZE = 16;
+
+const grid = document.querySelector(".grid");
+const input = document.querySelector(".sizeSelect");
+
+input.addEventListener("input", () =>{
+    clearGrid()
+    setupGrid(input.value);
+})
 function setupGrid(size){
-    const grid = document.querySelector(".grid");
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for(let i = 0; i < size*size; i++){
         const gridElem = document.createElement("div");
-        gridElem.style.backgroundColor = "black";
+        gridElem.style.backgroundColor = "white";
+        gridElem.style.border = "1px solid black"
         grid.appendChild(gridElem);
     }
 }
-
-setupGrid(16)
+function clearGrid(){
+    while(grid.hasChildNodes()){
+        grid.removeChild(grid.firstChild);
+    }
+}
+window.onload = () => {
+    setupGrid(GRID_SIZE);
+}
